a5a4d34fbfe5afefe624bac9366b96f1a0409923
Felipe Guimarães <felipeguimaraes@MacBook-Pro-de-Felipe.local>
2022-02-17 23:01:23 -0300

    first commit

---
+++ unicloud_server/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unicloud_server project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unicloud_server.settings')
+
+application = get_asgi_application()
