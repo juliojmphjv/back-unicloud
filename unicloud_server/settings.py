03edef6b6a9f3b35dcf6f19da953e4f34057db4d
Felipe Guimarães <felipeguimaraes@MacBook-Pro-de-Felipe.local>
2022-03-03 15:58:50 -0300

    commiting settings

--- unicloud_server/settings.py
+++ unicloud_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'unicloud_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unicloud',
+        'USER': 'postgres',
+        'PASSWORD': '1234@Collision',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
