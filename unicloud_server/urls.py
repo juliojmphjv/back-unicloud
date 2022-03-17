9a4960d7fcedba80ce94896cd83e5a16f404b703
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-03-17 19:12:35 -0300

    login defined

--- unicloud_server/urls.py
+++ unicloud_server/urls.py
@@ -20,11 +20,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-from unicloud_users.views import UserAPIView
+from unicloud_users.views import UserAPIView, MyTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/user/', UserAPIView.as_view(), name='user'),
