4a822181c0f8cfd8bddb86028cef5a1ca2fc985f
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-04-13 12:10:03 -0300

    adding origin

--- unicloud_server/settings.py
+++ unicloud_server/settings.py
@@ -201,6 +201,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://unicloudbr.azurewebsites.net"
 ]
 
 APIKEY_MAILGUN = os.getenv('API_MAILGUN')
