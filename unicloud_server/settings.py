56ee872bb4d4b1c4e370990213debff264dbfa40
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-04-18 14:54:23 -0300

    static root

--- unicloud_server/settings.py
+++ unicloud_server/settings.py
@@ -149,6 +149,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
