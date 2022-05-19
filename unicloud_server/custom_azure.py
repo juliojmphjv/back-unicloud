e74a3d2a9d7873f8894636b1aff765ebfbed7204
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-05-19 17:06:32 -0300

    fix key exposure

--- unicloud_server/custom_azure.py
+++ unicloud_server/custom_azure.py
@@ -1,14 +1,14 @@
-
 from storages.backends.azure_storage import AzureStorage
+from django.conf import settings
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'brokermediastorage' # Must be replaced by your <storage_account_name>
-    account_key = 'gxXiOCisdOZ8NaH8FW9ZDmJdbJfDMVjM0j9X/9+BlmHcG4AhJkx3wvsZiaSmDlAmPtizQnkMp9Xs/69TVQ9PDA==' # Must be replaced by your <storage_account_key>
+    account_key = settings.AZURE_ACCOUNT_KEY
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'brokermediastorage' # Must be replaced by your storage_account_name
-    account_key = 'gxXiOCisdOZ8NaH8FW9ZDmJdbJfDMVjM0j9X/9+BlmHcG4AhJkx3wvsZiaSmDlAmPtizQnkMp9Xs/69TVQ9PDA==' # Must be replaced by your <storage_account_key>
+    account_key = settings.AZURE_ACCOUNT_KEY
     azure_container = 'static'
     expiration_secs = None
