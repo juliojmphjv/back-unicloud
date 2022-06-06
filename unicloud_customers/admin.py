9df3e18736aa35ecbd25a0dd48c50dd81468e01b
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-06-06 17:26:56 -0300

    fiz update logo

--- unicloud_customers/admin.py
+++ unicloud_customers/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Customer, CustomerRelationship, UserCustomer, InvitedUser
+from .models import Customer, CustomerRelationship, UserCustomer, InvitedUser, OrganizationLogo
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(CustomerRelationship)
 admin.site.register(UserCustomer)
 admin.site.register(InvitedUser)
+admin.site.register(OrganizationLogo)
