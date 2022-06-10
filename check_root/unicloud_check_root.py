f35c9a7675cb2bebaf22c21cc27cb6d2af7e05e5
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-06-10 17:58:04 -0300

    more logs

--- check_root/unicloud_check_root.py
+++ check_root/unicloud_check_root.py
@@ -8,6 +8,10 @@ class CheckRoot:
 
     def is_root(self):
         organization = Customer.objects.get(id=self.__user_customer_instance.objects.get(user_id=self.__request.user.id).customer_id)
+        logger.info(f'user is staff: {self.__request.user.is_staff}')
+        logger.info(f'user is superuser: {self.__request.user.is_superuser}')
+        logger.info(f'customer is root: {self.__request.user.is_staff}')
+
         if self.__request.user.is_staff and self.__request.user.is_superuser and organization.type == "root" and organization.razao_social == "Uni.Cloud" :
             logger.info("Requester is root!")
             return True
