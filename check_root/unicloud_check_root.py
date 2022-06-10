ac3489dec73dd0fa8d77c51833f830239a3b47b9
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-06-10 18:15:33 -0300

    more logs

--- check_root/unicloud_check_root.py
+++ check_root/unicloud_check_root.py
@@ -3,17 +3,16 @@ from logs.setup_log import logger
 
 class CheckRoot:
     def __init__(self, request):
-        self.__request=request
-        self.__user_customer_instance = UserCustomer
+        self.request=request
+        self.user_customer_instance = UserCustomer
 
     def is_root(self):
-        organization = Customer.objects.get(id=self.__user_customer_instance.objects.get(user_id=self.__request.user.id).customer_id)
-        logger.info(f'user is staff: {self.__request.user.is_staff}')
-        logger.info(f'user is superuser: {self.__request.user.is_superuser}')
-        logger.info(f'customer is root: {self.__request.user.is_staff}')
+        organization = Customer.objects.get(id=self.user_customer_instance.objects.get(user_id=self.request.user.id).customer_id)
+        logger.info(f'user is staff: {self.request.user.is_staff}')
+        logger.info(f'user is superuser: {self.request.user.is_superuser}')
+        logger.info(f'customer is root: {self.request.user.is_staff}')
 
-        if self.__request.user.is_staff and self.__request.user.is_superuser and organization.type == "root" and organization.razao_social == "Uni.Cloud" :
-            logger.info("Requester is root!")
+        if self.request.user.is_staff and self.request.user.is_superuser and organization.type == "root" and organization.razao_social == "Uni.Cloud" :
             return True
         return False
 
