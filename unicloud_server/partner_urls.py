a6d5293aeef7c7447d60e24305cedcbbbe0c0b1e
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Air.local>
2022-08-16 10:30:40 -0300

    create customer history

--- unicloud_server/partner_urls.py
+++ unicloud_server/partner_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from unicloud_sales.api.viewset import OneOpportunity
+from unicloud_sales.api.viewset import OneOpportunity, CustomerSalesHistory
 from unicloud_dashboard.api.viewset import PartnerDashboard
 
 urlpatterns = [
     path('get-one-opportunity/', OneOpportunity.as_view({'get': 'get_one_opportunity'})),
     path('dashboard/', PartnerDashboard.as_view({'get': 'get_dashboard'})),
+    path('create-history/',CustomerSalesHistory.as_view({'post': 'create_customer_activity'})),
 ]
